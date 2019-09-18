91dadc06a4cbc34f2dd5e98b96c92a917bb2bda6
xueshaoyan <1135651994@qq.com>
2019-09-18 19:21:38 +0800

    第一次牵你的手

---
+++ src/main/java/com/xsy/githuptest/GithuptestApplication.java
@@ -0,0 +1,13 @@
+package com.xsy.githuptest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithuptestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithuptestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xsy/githuptest/GithuptestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xsy.githuptest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GithuptestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
