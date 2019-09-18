381e38de88b437744ccd62f9b10e3e1401ff2bd9
xueshaoyan <1135651994@qq.com>
2019-09-18 19:47:23 +0800

    第二次牵你的手

--- src/main/java/com/xsy/githuptest/GithuptestApplication.java
+++ src/main/java/com/xsy/githuptest/GithuptestApplication.java
@@ -1,32 +1,7 @@
 package com.xsy.githuptest;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class GithuptestApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(GithuptestApplication.class, args);
-    }
-
-
-    static class sj(){
-
-    }
-
-    public void show(){
-       class sj(){
-
-        }
-
-        new object(){
-            public void show(){
-                System.out.println("nighaoo");
-            }
-        }.show();
-
-    }
+public class test {
 
 
 }
